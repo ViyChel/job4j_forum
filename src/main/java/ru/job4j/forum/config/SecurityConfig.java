ec230e24d05a6c19e9021bfddf2418ced8378d45
Vitaly Yagufarov <for.viy@gmail.com>
2021-05-20 21:25:45 +0500

    add access to posts anonymous users

--- src/main/java/ru/job4j/forum/config/SecurityConfig.java
+++ src/main/java/ru/job4j/forum/config/SecurityConfig.java
@@ -44,7 +44,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/login", "/reg", "/", "/resources/**")
+                .antMatchers("/post/new").authenticated()
+                .antMatchers("/login", "/reg", "/", "/post/**", "/resources/**")
                 .permitAll()
                 .anyRequest().authenticated()
                 .and()
